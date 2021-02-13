25c6b6f910cb5eb877f26bcbfb7eb1a9ff436069
a-taghva <taghvali99@gmail.com>
2021-02-13 12:27:19 -0500

    pass tests for Engineer

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,16 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates engineer object', () => {
+    const engineer = new Engineer('Sara', '3', 'sara123@gmail.com', 'sara-git');
+
+    expect(engineer.name).toBe('Sara');
+    expect(engineer.id).toBe('3');
+    expect(engineer.email).toBe('sara123@gmail.com');
+    expect(engineer.github).toBe('sara-git');
+});
+
+test('get engineer\'s role', () => {
+    const engineer = new Engineer('Sara', '3', 'sara123@gmail.com', 'sara-git');
+
+    expect(engineer.getRole()).toBe('Engineer');
+});
