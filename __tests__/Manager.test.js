fd5e8a38115f31e9e8e7ff3b0c9db1f5d727dbe1
a-taghva <taghvali99@gmail.com>
2021-02-13 12:14:11 -0500

    add and pass tests for Manager

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require('../lib/Manager');
+
+test('creates Manager object', () => {
+    const manager = new Manager('Alex', '2', 'alex123@gmail.com', '415');
+
+    expect(manager.name).toBe('Alex');
+    expect(manager.id).toBe('2');
+    expect(manager.email).toBe('alex123@gmail.com');
+    expect(manager.officeNumber).toBe('415');
+});
+
+test('get manager\'s role', () => {
+    const manager = new Manager('Alex', '2', 'alex123@gmail.com', '415');
+
+    expect(manager.getRole()).toBe('Manager');
+})
