2a91b06d45307c7a135ac2825c85a3beefbfe822
a-taghva <taghvali99@gmail.com>
2021-02-13 12:36:41 -0500

    add and pass tests for Intern

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require('../lib/Intern');
+
+test('create intern object', () => {
+    const intern = new Intern('Robin', '4', 'robin123@gmail.com', 'uoft');
+
+    expect(intern.name).toBe('Robin');
+    expect(intern.id).toBe('4');
+    expect(intern.email).toBe('robin123@gmail.com');
+    expect(intern.school).toBe('uoft');
+});
+
+test('get intern\'s role', () => {
+    const intern = new Intern('Robin', '4', 'robin123@gmail.com', 'uoft');
+
+    expect(intern.getRole()).toBe('Intern');
+});
+
+test('get intern\'s school', () => {
+    const intern = new Intern('Robin', '4', 'robin123@gmail.com', 'uoft');
+
+    expect(intern.getSchool()).toBe(intern.school);
+})
